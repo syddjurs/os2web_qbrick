73896c680c9d3844d0adf8bed29a76c4dc54246c
Anders Bryrup <andersbryrup@gmail.com>
2014-03-28 12:40:12 +0100

    SYDDK-85 Be sure to wrap JS in Drupal Behaviours. Add JS to site on attached.

--- js/os2web_qbrick.js
+++ js/os2web_qbrick.js
@@ -3,11 +3,30 @@
  */
 
 (function ($) {
-  var widget = Qbrick.Publisher.widget().firstOrDefault('(w, index, additionalParam) => \'player\' === w.widgetId && (console.log(additionalParam) || true)', 'additionalParamValue');
-  function play(amount) {
-    if (widget) {
-      widget.position(amount);
-      widget.play();
+  var qbrick_widget;
+  Drupal.behaviors.os2web_qbrick = {
+    attach: function(context, settings) {
+      var $player = $('#qbrick');
+      Qbrick.Publisher.create(document.body, {
+        mcid : $player.data('qbrick-mcid'),
+        widgetId : 'qbrick',
+        widgetType : 'professional',
+        height: 180,
+        width: 320,
+      });
+      $('.qbrick-seek').click(function(e) {
+        Drupal.behaviors.os2web_qbrick.play($(this).data('qbrick-seek'));
+      });
+    },
+    play: function(amount) {
+      if (!qbrick_widget) {
+        qbrick_widget = Qbrick.Publisher.widgets().qbrick;
+      }
+      if (qbrick_widget) {
+        qbrick_widget.position(amount);
+        qbrick_widget.play();
+      }
     }
-  }
+  };
+
 })(jQuery);
